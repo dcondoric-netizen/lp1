12c19b6f341c7f307a5fefc0fbe4ee7a548bb4bd
dcondoric-netizen <dcondoric@ulasalle.edu.pe>
2026-09-03 14:11:21 -0500

    Update raizDigital.cpp

--- unidad1/lab02/raizDigital.cpp
+++ unidad1/lab02/raizDigital.cpp
@@ -1,1 +1,21 @@
+#include <iostream>
 
+using namespace std;
+//Deyvis Beltran Condori Coyla
+int main() {
+    int num;
+    while (cin >> num && num != 0) {
+    
+        while (num >= 10) {
+            int suma = 0;
+            while (num > 0) {
+                int dig = num % 10;
+                suma = suma + dig;  
+                num = num / 10;        
+            }
+            num = suma; 
+        }
+        cout << num << "\n";
+    }
+    return 0;
+}
